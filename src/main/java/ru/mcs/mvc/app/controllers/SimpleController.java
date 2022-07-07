2b4044b0bd3ffa9f0c8f7f02bc5d091fca9de5bb
makhmud-shafeev <shafeevmkf@gmail.com>
2022-07-07 21:48:25 +0300

    example get HttpServletRequest params

--- src/main/java/ru/mcs/mvc/app/controllers/SimpleController.java
+++ src/main/java/ru/mcs/mvc/app/controllers/SimpleController.java
@@ -4,17 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @RequestMapping("/first")
 public class SimpleController {
 
     @GetMapping("/simple")
-    public String simple(){
+    public String simple() {
         return "simple";
     }
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(HttpServletRequest request) {
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+
+        System.out.println("hello " + name + " " + surname);
+
         return "first/hello";
     }
 
